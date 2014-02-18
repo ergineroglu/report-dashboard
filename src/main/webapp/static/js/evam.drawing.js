f8a373dfab587e51613f5b06d638cbd116c83300
Ergin EROĞLU <eeroglu@gmail.com>
2014-02-18 15:51:56 +0200

    areachart and areachartbrush corrections

--- src/main/webapp/static/js/evam.drawing.js
+++ src/main/webapp/static/js/evam.drawing.js
@@ -1,5 +1,17 @@
 evam = window.evam || {};
 
+// General Functions
+evam.customTimeFormat = d3.time.format.multi([
+	[".%L", function(d) { return d.getMilliseconds(); }],
+	[":%S", function(d) { return d.getSeconds(); }],
+	["%I:%M", function(d) { return d.getMinutes(); }],
+	["%I %p", function(d) { return d.getHours(); }],
+	["%a %d", function(d) { return d.getDay() && d.getDate() != 1; }],
+	["%b %d", function(d) { return d.getDate() != 1; }],
+	["%b", function(d) { return d.getMonth(); }],
+	["%Y", function() { return true; }]
+]);
+
 // Graph 
 evam.Graph = function(key, title, xAxis, yAxis, requestUrl, requestData){ 
 	this.key				= key;
@@ -23,7 +35,7 @@ evam.Graph.prototype.draw = function() {
 evam.Areachart = function(key, title, xAxis, yAxis, requestUrl, requestData){ 
 	evam.Graph.call(this, key, title, xAxis, yAxis, requestUrl, requestData);	
 	this.outerHeight		= 500;
-	this.margin 			= {top: 5, right: 10, bottom: 90, left: 45};
+	this.margin 			= {top: 5, right: 10, bottom: 20, left: 45};
 	this.width 				= this.outerWidth - this.margin.left - this.margin.right;
     this.height 			= this.outerHeight - this.margin.top - this.margin.bottom;
     this.parseX 			= d3.time.format("%d.%m.%Y").parse;
@@ -33,7 +45,7 @@ evam.Areachart.prototype.initialize = function() {
 	var areaChart 			= this;
 	this.x 					= d3.time.scale().range([0, this.width]);
 	this.y 					= d3.scale.linear().range([this.height, 0]);
-	this.xAxis 				= d3.svg.axis().scale(this.x).orient("bottom").tickFormat(d3.time.format("%Y.%m.%d"));
+	this.xAxis 				= d3.svg.axis().scale(this.x).orient("bottom").tickFormat(evam.customTimeFormat);
 	this.yAxis 				= d3.svg.axis().scale(this.y).orient("left");
 	this.area 				= d3.svg.area().x(function(d) { return areaChart.x(d.x); })
     									   .y0(areaChart.height)
@@ -53,16 +65,14 @@ evam.Areachart.prototype.draw = function() {
 		// sort data and extend domains	
 		data.sort(function(a, b) { return a.x - b.x; });
 		areaChart.x.domain([data[0].x, data[data.length - 1].x]);
-		areaChart.y.domain([0, d3.max(data, function(d) { return d.y; })]);
+		areaChart.y.domain([0, 1.1 * d3.max(data.map(function(d) { return d.y; }))]);
+		// area
+		areaChart.svg.append("path").datum(data).attr("class", "area").attr("d", areaChart.area);
 		// axises		
 		areaChart.svg.append("g").attr("class", "x axis").attr("transform", "translate(0," + areaChart.height + ")").call(areaChart.xAxis);
-		areaChart.svg.selectAll("text").style("text-anchor", "end").attr("dx", "-.8em").attr("dy", ".15em")
-	 	  						       .attr("transform", function(d) { return "rotate(-70)"; });
 		areaChart.svg.append("text").attr("x", areaChart.width - 6).attr("y",  areaChart.height - 6).style("text-anchor", "end").text(areaChart.xAxisTitle);
 		areaChart.svg.append("g").attr("class", "y axis").call(areaChart.yAxis);
 		areaChart.svg.append("text").attr("transform", "rotate(-90)").attr("y", 6).attr("dy", ".71em").style("text-anchor", "end").text(areaChart.yAxisTitle);
-		// area
-		areaChart.svg.append("path").datum(data).attr("class", "area").attr("d", areaChart.area);	
 		// mousemove function
 		var bisectY = d3.bisector(function(d) { return d.x; }).left;
 		var mousemove = function() {
@@ -76,8 +86,8 @@ evam.Areachart.prototype.draw = function() {
 		};
 		// focus object for point information
 		var focus = areaChart.svg.append("g").attr("class", "focus").style("display", "none");
-		focus.append("circle").attr("r", 10);
-		focus.append("text").attr("x", 12).attr("dy", ".8em");
+		focus.append("circle").attr("r", 10).attr("stroke-width", 2);
+		focus.append("text").attr("x", 0).attr("y", -12).style("fill", "red").style("text-anchor", "middle").style("font-size", "12px").style("font-weight", "bold");
 		areaChart.svg.append("rect").attr("class", "overlay").attr("width", areaChart.width).attr("height", areaChart.height)
 	       		     .on("mouseover", function() { focus.style("display", null); })
        			     .on("mouseout", function() { focus.style("display", "none"); })
@@ -91,33 +101,33 @@ evam.Areachart.prototype.draw = function() {
 evam.AreachartBrush = function(key, title, xAxis, yAxis, requestUrl, requestData){ 
 	evam.Areachart.call(this, key, title, xAxis, yAxis, requestUrl, requestData);		
 	this.margin 			= {top: 5, right: 10, bottom: 100, left: 45};
-	this.margin2 			= {top: 430, right: 10, bottom: 20, left: 45};	
+	this.margin2 			= {top: 430, right: 10, bottom: 20, left: 45};
+	this.width 				= this.outerWidth - this.margin.left - this.margin.right;
+    this.height 			= this.outerHeight - this.margin.top - this.margin.bottom;
     this.height2 			= this.outerHeight - this.margin2.top - this.margin2.bottom;    
 }; 
 evam.AreachartBrush.prototype = Object.create(evam.Areachart.prototype);
 evam.AreachartBrush.prototype.initialize = function() { 	
 	var areaChartBrush 		= this;
 	this.x 					= d3.time.scale().range([0, this.width]);
-	this.y 					= d3.scale.linear().range([this.height, 0]);
 	this.x2 				= d3.time.scale().range([0, this.width]);
+	this.y 					= d3.scale.linear().range([this.height, 0]);	
 	this.y2 				= d3.scale.linear().range([this.height2, 0]);
-	this.xAxis				= d3.svg.axis().scale(this.x).orient("bottom"),
-	this.xAxis2 			= d3.svg.axis().scale(this.x2).orient("bottom");
-	this.yAxis				= d3.svg.axis().scale(this.y).orient("left");
-	this.area 				= d3.svg.area().interpolate("monotone")
-										   .x(function(d) { return areaChartBrush.x(d.x); })
+	this.xAxis				= d3.svg.axis().scale(this.x).orient("bottom").tickFormat(evam.customTimeFormat);
+	this.xAxis2 			= d3.svg.axis().scale(this.x2).orient("bottom").tickFormat(evam.customTimeFormat);
+	this.yAxis				= d3.svg.axis().scale(this.y).orient("left");	
+	this.area 				= d3.svg.area().x(function(d) { return areaChartBrush.x(d.x); })
     									   .y0(areaChartBrush.height)
     									   .y1(function(d) { return areaChartBrush.y(d.y); });
-	this.area2 				= d3.svg.area().interpolate("monotone")
-    							 		   .x(function(d) { return areaChartBrush.x2(d.x); })
+	this.area2 				= d3.svg.area().x(function(d) { return areaChartBrush.x2(d.x); })
     							 		   .y0(areaChartBrush.height2)
-    							 		   .y1(function(d) { return areaChartBrush.y2(d.price); });
+    							 		   .y1(function(d) { return areaChartBrush.y2(d.y); });
 	// brushed function
 	this.brushed = function() {
 		areaChartBrush.x.domain(areaChartBrush.brush.empty() ? areaChartBrush.x2.domain() : areaChartBrush.brush.extent());
 		areaChartBrush.focus.select(".area").attr("d", areaChartBrush.area);
 		areaChartBrush.focus.select(".x.axis").call(areaChartBrush.xAxis);
-	};		
+	};
 	this.brush 				= d3.svg.brush().x(this.x2).on("brush", this.brushed);
 	this.svg 				= d3.select(this.jqueryObject[0]).append("svg").attr("width", this.outerWidth).attr("height", this.outerHeight);	
 	this.focus 				= this.svg.append("g").attr("class", "focus").attr("transform", "translate(" + areaChartBrush.margin.left + "," + areaChartBrush.margin.top + ")");
@@ -133,19 +143,18 @@ evam.AreachartBrush.prototype.draw = function() {
 			d.x = areaChartBrush.parseX(d.x);
 		    d.y = +d.y;
 		});
-		// sort data and extend domains	
-		data.sort(function(a, b) { return a.x - b.x; });
-		areaChartBrush.x.domain([data[0].x, data[data.length - 1].x]);
-		areaChartBrush.y.domain([0, d3.max(data, function(d) { return d.y; })]);
+		// extend domains
+		areaChartBrush.x.domain(d3.extent(data.map(function(d) { return d.x; })));
+		areaChartBrush.y.domain([0, 1.1 * d3.max(data.map(function(d) { return d.y; }))]);
 		areaChartBrush.x2.domain(areaChartBrush.x.domain());
-		areaChartBrush.y2.domain(areaChartBrush.y.domain());		
-		// area
-		areaChartBrush.focus.append("path").datum(data).attr("class", "area").attr("d", areaChartBrush.area);	
-		// axises		
+		areaChartBrush.y2.domain(areaChartBrush.y.domain());				
+		// upper graph
+		areaChartBrush.focus.append("path").datum(data).attr("class", "area").attr("d", areaChartBrush.area);
 		areaChartBrush.focus.append("g").attr("class", "x axis").attr("transform", "translate(0," + areaChartBrush.height + ")").call(areaChartBrush.xAxis);
 		areaChartBrush.focus.append("text").attr("x", areaChartBrush.width - 6).attr("y",  areaChartBrush.height - 6).style("text-anchor", "end").text(areaChartBrush.xAxisTitle);
 		areaChartBrush.focus.append("g").attr("class", "y axis").call(areaChartBrush.yAxis);
 		areaChartBrush.focus.append("text").attr("transform", "rotate(-90)").attr("y", 6).attr("dy", ".71em").style("text-anchor", "end").text(areaChartBrush.yAxisTitle);
+		// lower graph		
 		areaChartBrush.context.append("path").datum(data).attr("class", "area").attr("d", areaChartBrush.area2);
 		areaChartBrush.context.append("g").attr("class", "x axis").attr("transform", "translate(0," + areaChartBrush.height2 + ")").call(areaChartBrush.xAxis2);
 		areaChartBrush.context.append("g").attr("class", "x brush").call(areaChartBrush.brush)
